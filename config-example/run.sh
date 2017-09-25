86f017f678942efa6db24f4ba6d2281ca241102d
allofmex <allofmex@web.de>
2017-09-25 13:23:50 +0200

    Update run.sh

--- config-example/run.sh
+++ config-example/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+TARGETPATH_ALL="/hostTarget/photos_all"
+TARGETPATH_LAND="/hostTarget/photos_landscape"
+
 #we need to know our own path (for later)
 MY_PATH="`dirname \"$0\"`"
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
@@ -15,8 +18,8 @@ set -x
 # SIGTERM-handler to umount on container exit
 term_handler() {
   echo "umount cmdfs mount"
-  umount -f /hostTarget/photos_landscape
-  umount -f /hostTarget/photos_all
+  umount -f $TARGETPATH_LAND
+  umount -f $TARGETPATH_ALL
   exit 143; # 128 + 15 -- SIGTERM
 }
 
@@ -33,8 +36,8 @@ echo "mount cmdfs"
 #cmdfs /hostSource /hostTarget -s -o "allow_other,command=convert - -resize 1920x1080 -,mime-re=image/*,cache-dir=$CACHE_DIR,monitor,cache-size=500"
 
 #-s important, single threaded. If not used, cmdfs crashes after a while
-cmdfs /hostSource /hostTarget/photos_all -s -o "extension=jpg;jpeg;gif;png,monitor,command=$MY_PATH/img_filter.sh,cache-dir=$CACHE_DIR,cache-size=$CACHE,allow_other"
-cmdfs /hostTarget/photos_all /hostTarget/photos_landscape -s \
+cmdfs /hostSource $TARGETPATH_ALL -s -o "extension=jpg;jpeg;gif;png,monitor,command=$MY_PATH/img_filter.sh,cache-dir=$CACHE_DIR,cache-size=$CACHE,allow_other"
+cmdfs /hostTarget/photos_all $TARGETPATH_LAND -s \
   -o "cache-dir=/tmp,cache-entries=2,exclude-re=.*[#][L][5-9].*,extension=jpg;jpeg;gif;png,hide-empty-dirs,command=$MY_PATH/img_filter_land.sh,allow_other"
 
 # wait forever (to keep container running)
